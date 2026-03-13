c221eb208cf290ff58711891dd288a36a2afb0ef
tanabeyutotech-tech <tanabeyuto.tech@gmail.com>
2026-03-13 08:50:15 +0100

    feat: config fixed

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+
+HEADERS = {
+    "Authorization": f"token {GITHUB_TOKEN}",
+    "Accept": "application/vnd.github+json",
+    "User-Agent": "Mozilla/5.0"
+}
